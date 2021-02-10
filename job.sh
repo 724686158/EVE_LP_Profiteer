4ade526f9906c88783f1860b950edce56438224d
724686158 <solitarius.holic@gmail.com>
2021-02-10 16:31:04 +0800

    data update

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/EVE_LP_Profiteer/
-python3 /root/EVE_LP_Profiteer/SerpentisLP.py
+python3 /root/EVE_LP_Profiteer/LP_DATA.py
 git add -A
 git commit -m "data update"
 git push -f
